380d19d624b0475272e6b1b63a6c9a9f71098997
nosti <julie.hubert@epfedu.fr>
2024-03-06 22:23:40 +0100

    ajout du CSS loader

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -2,7 +2,8 @@ import { Router } from "./app/scripts/router";
 import { WelcomeComponent } from "./app/scripts/welcome";
 import { GameComponent } from "./app/scripts/game";
 import { ScoreComponent } from "./app/scripts/score";
-// TODO #import-css: use ES side-effect imports to import styles/style.css
+import "./../node_modules/bootstrap/dist/css/bootstrap.css";
+import "./app/styles/style.css";
 
 const outlet = document.querySelector("#content-outlet");
 const router = new Router(outlet);
